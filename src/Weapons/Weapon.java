2e8cb73f7342cfbe4fc78c507c90db3ba1a2aacb
Brady Galligan <brady.galligan@gmail.com>
2024-11-02 22:55:08 -0400

    Got a beta of the weapons finished, player can now take damage, weapons won't graphically render in

--- src/Weapons/Weapon.java
+++ src/Weapons/Weapon.java
@@ -1,17 +1,41 @@
 package Weapons;
 
+import java.util.HashMap;
+
+import Builders.FrameBuilder;
+import Engine.ImageLoader;
+import Engine.MouseHandler;
+import GameObject.Frame;
 import GameObject.GameObject;
+import GameObject.SpriteSheet;
+import Level.Map;
+import Level.MapEntity;
+import Level.Player;
 import Utils.Point;
 
-public class Weapon extends GameObject{
-    private int fireRate;
-    private int DPH;
-    public Weapon(int fireRate, int DPH, Point playerLoc) {
-        super(playerLoc.x, playerLoc.y); 
-        this.fireRate = fireRate;
-        this.DPH = DPH;
+public class Weapon extends MapEntity{
+    protected int fireRate;
+    protected float DPH; //Damage per hit
+    protected float antiJankTimer, bulletSpeed;
+    protected Map map;
+    
+    public Weapon(Frame img, Point playerLoc, Map map) { //Copy paste this constructor into your subclass
+        super(playerLoc.x, playerLoc.y, img);
+        this.map = map; 
+        //Input value for subclass weapon here
+        this.fireRate = 0; 
+        this.DPH = 0;
+        this.bulletSpeed = 0;
+        antiJankTimer = 0;
     }
-    public void shoot() {
-
+    public void shoot(float Sx, float Sy, float Fx, float Fy) {
+        if ((System.nanoTime()-antiJankTimer)/1000000000.0 > fireRate) {
+            System.out.println("DEBUG: shot");
+            antiJankTimer = System.nanoTime();
+            new Bullet(new SpriteSheet(ImageLoader.load("tempEnemy.png"), 16, 16), Sx, Sy, Fx, Fy, bulletSpeed, map);
+        }
+    }
+    public void update(Player player) {
+        this.setLocation(player.getX(), player.getY()); 
     }
 }
