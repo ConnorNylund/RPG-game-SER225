6fe16325a10a792e71ca759155ab707aa380e3a7
riatmax <riatmax@gmail.com>
2024-11-14 20:08:57 -0500

    made some things static

--- src/Players/Bunny.java
+++ src/Players/Bunny.java
@@ -23,8 +23,8 @@ import Weapons.TestPistol;
 import Weapons.*;
 
 public class Bunny extends Player {
-    private float health;
-    private int dmgState;
+    public static float health;
+    public static int dmgState;
     private Weapon currentWeapon;
     private boolean test;
     public static int coinCount = 0; // this is to track coins
