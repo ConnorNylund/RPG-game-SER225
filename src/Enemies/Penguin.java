69c2481d0a2d176c3c6be5b2286e83c74f0ba477
Brady Galligan <brady.galligan@gmail.com>
2024-11-14 19:41:31 -0500

    Made Penguin started working on NPC door things

---
+++ src/Enemies/Penguin.java
@@ -0,0 +1,18 @@
+package Enemies;
+
+import Engine.ImageLoader;
+import GameObject.SpriteSheet;
+import Level.Player;
+import Utils.Point;
+
+public class Penguin extends Enemy{
+    private static SpriteSheet spriteSheet = new SpriteSheet(ImageLoader.load("penguinsprites.png"), 16, 16);
+    public Penguin(int id, Point location, String startingAnimation, Player player) {
+        super(id, location, spriteSheet, startingAnimation);
+        this.id = id; 
+        moveSpeed = 2.0f;
+        attackRadius = 60; 
+        totalHealth = 3;
+        attackSpeed = 3; 
+    }
+}
