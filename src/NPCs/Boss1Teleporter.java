a68ed5a0df5e1e31c192f54cb390fe15f3f5beeb
Brady Galligan <brady.galligan@gmail.com>
2024-11-18 16:27:49 -0500

    Added bow, made the NPC for the BossTeleporter... sort of

--- src/NPCs/Boss1Teleporter.java
+++ src/NPCs/Boss1Teleporter.java
@@ -1,5 +1,12 @@
 package NPCs;
 
-public class Boss1Teleporter {
-    
+import Engine.ImageLoader;
+import GameObject.SpriteSheet;
+import Level.NPC;
+
+public class Boss1Teleporter extends NPC {
+    static SpriteSheet spriteSheet = new SpriteSheet(ImageLoader.load("Sword.png"), 16, 16); 
+        public Boss1Teleporter(int id, float x, float y, String startingAnim) {
+            super(id, x, y, spriteSheet, startingAnim); 
+    }
 }
--- src/Weapons/Bow.java
+++ src/Weapons/Bow.java
@@ -1,9 +1,12 @@
 package Weapons;
 
+import Engine.ImageLoader;
+import GameObject.SpriteSheet;
 import Level.Map;
 import Utils.Point;
 
 public class Bow extends Weapon {
+    private static SpriteSheet gunPic = new SpriteSheet(ImageLoader.load("Bow.png"), 16, 16);
     public Bow(Point playerLoc, Map map) {
         super(gunPic, playerLoc, map);
         this.fireRate = 3f;
